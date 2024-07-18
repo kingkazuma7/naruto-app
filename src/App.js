d603858661b9da4f270f9097eb0309126118fe80
kingkazuma7 <your.nasionasio7@gmail.com>
2024-07-18 21:02:22 +0900

    10. axiosでReactからAPIをコールする
    - axios
    - React内で
    - apiコール

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
+import { useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  useEffect(() => {
+    fecthCharacters();
+  }, [])
+
+  const fecthCharacters = async () => {
+    const apiUrl = 'https://narutodb.xyz/api/character'
+    // axios getメソッドでapiUrlにリクエストを送る
+    const result = await axios.get(apiUrl);
+    console.log(result);
+  };
+
   return (
     <div className="App">
       hello world
