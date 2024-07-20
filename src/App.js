a746f3597c1be62aa2fc07e4b6060cea69e2b338
kingkazuma7 <your.nasionasio7@gmail.com>
2024-07-20 11:33:47 +0900

    12. 取得したデータ表示 - fecthCharacters関数   - url   - useStateの更新関数に値セット

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import axios from 'axios';
 
 function App() {
-  useEffect(() => {
+  // 画面に表示する
+  const [characters, setCharacters] = useState([]); // キャラクターの状態管理
+
+  useEffect(() => { // 副作用: APIからのデータ取得
     fecthCharacters();
   }, [])
 
@@ -12,11 +15,25 @@ function App() {
     // axios getメソッドでapiUrlにリクエストを送る
     const result = await axios.get(apiUrl);
     console.log(result);
+
+    // 更新関数で取得したキャラクターのURLに値を更新する
+    setCharacters(result.data.characters);
   };
 
   return (
-    <div className="App">
-      hello world
+    <div className="container">
+      <main>
+        <div className="cards-container">
+          {characters.map((characters) => {
+            return <div className='card' key={characters.id}>
+              <img src={characters.images[0]
+                ? characters.images[0]
+                : 'dummy.png'}
+                alt="characters" className='card-image' />
+            </div>;
+          })}
+        </div>
+      </main>
     </div>
   );
 }
