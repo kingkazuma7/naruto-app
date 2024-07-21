b617658990707f08fddea20784c760d20afd9a01
kingkazuma7 <your.nasionasio7@gmail.com>
2024-07-21 09:03:13 +0900

    14. ローディング
    - useState pagerでローディング操作

--- src/App.js
+++ src/App.js
@@ -3,8 +3,19 @@ import './App.css';
 import axios from 'axios';
 
 function App() {
-  // 画面に表示する
   const [characters, setCharacters] = useState([]); // キャラクターの状態管理
+  const [page, setPage] = useState(1) // pagerの状態管理、初期値は1ページ目の1
+
+  // ページャーカウントアップ
+  const handleNext = async () => {
+    const nextPage = page + 1 // カウントアップ
+    await fecthCharacters(nextPage); // fecthCharacters呼び出し、引数で次ページ
+    setPage(nextPage); // 状態変数で更新
+  }
+  
+  // ローディング
+  const [isLoading, setIsLoading] = useState(false); 
+  // APIたたく→取り直す直前にstateをtrue→とれたらfalse
 
   useEffect(() => { // 副作用: APIからのデータ取得
     fecthCharacters(); // 2,3などpageの数値がわたってくる
@@ -13,51 +24,52 @@ function App() {
 
   const fecthCharacters = async (page) => {
     const apiUrl = 'https://narutodb.xyz/api/character'
-    // axios getメソッドでapiUrlにリクエストを送る
-    const result = await axios.get(apiUrl, { params: { page: page } });
-    // console.log(result);
-
+    setIsLoading(true);
+    const result = await axios.get(apiUrl, { params: { page: page } }); // axios getメソッドでapiUrlにリクエストを送る
     // 更新関数で取得したキャラクターのURLに値を更新する
     setCharacters(result.data.characters);
+    setIsLoading(false);
   };
 
   return (
     <div className="container">
-      <main>
-        <div className="cards-container">
-          {characters.map((characters) => {
-            return (
-              <div className='card' key={characters.id}>
-                <img src={characters.images[0]
-                  ? characters.images[0]
-                  : 'dummy.png'}
-                  alt="characters" className='card-image' />
-                  <div className="card-content">
-                    <h3 className="card-title">{characters.name}</h3>
-                    {/* <p className="card-description">
-                      {characters.debut != null
-                        ? characters.debut.appearsIn
-                        : "なし" }
-                    </p> */}
-                    {/* appearsInあれば表示、なければ右辺 */}
-                    <p className="card-description">
-                      { characters.debut?.appearsIn ?? 'debutなし' }
-                    </p>
-                    <div className="card-footer">
-                      <span className="affiliation">
-                        { characters.personal?.species ?? 'personalなし' }</span>
+      { isLoading ? '読み込中...' :
+        <main>
+          <div className="cards-container">
+            {characters.map((characters) => {
+              return (
+                <div className='card' key={characters.id}>
+                  <img src={characters.images[0]
+                    ? characters.images[0]
+                    : 'dummy.png'}
+                    alt="characters" className='card-image' />
+                    <div className="card-content">
+                      <h3 className="card-title">{characters.name}</h3>
+                      {/* <p className="card-description">
+                        {characters.debut != null
+                          ? characters.debut.appearsIn
+                          : "なし" }
+                      </p> */}
+                      {/* appearsInあれば表示、なければ右辺 */}
+                      <p className="card-description">
+                        { characters.debut?.appearsIn ?? 'debutなし' }
+                      </p>
+                      <div className="card-footer">
+                        <span className="affiliation">
+                          { characters.personal?.species ?? 'personalなし' }</span>
+                      </div>
                     </div>
-                  </div>
-              </div>
-            );
-          })}
+                </div>
+              );
+            })}
+          </div>
           <div className='pager'>
             <button className='prev'>前へ</button>
-            <span className="pager-number">1</span>
-            <button className='next'>次へ</button>
+            <span className="pager-number">{page}</span>
+            <button className='next' onClick={handleNext}>次へ</button>
           </div>
-        </div>
-      </main>
+        </main>
+      }
     </div>
   );
 }
