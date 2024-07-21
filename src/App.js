c1b907309b3c1ab2aa676fe1d81a12d63fb7025f
kingkazuma7 <your.nasionasio7@gmail.com>
2024-07-21 09:30:08 +0900

    header add

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import axios from 'axios';
 function App() {
   const [characters, setCharacters] = useState([]); // キャラクターの状態管理
   const [page, setPage] = useState(1) // pagerの状態管理、初期値は1ページ目の1
+  const [isLoading, setIsLoading] = useState(false); // ローディング
+  const limit = 20; // ページに表示するキャラクター数, 全部で1400体
 
   // ページャーカウントアップ
   const handleNext = async () => {
@@ -12,27 +14,32 @@ function App() {
     await fecthCharacters(nextPage); // fecthCharacters呼び出し、引数で次ページ
     setPage(nextPage); // 状態変数で更新
   }
-  
-  // ローディング
-  const [isLoading, setIsLoading] = useState(false); 
-  // APIたたく→取り直す直前にstateをtrue→とれたらfalse
+  // ページャーカウントダウン
+  const handlePrev = async () => {
+    const prevPage = page - 1;
+    await fecthCharacters(prevPage);
+    setPage(prevPage);
+  }
 
   useEffect(() => { // 副作用: APIからのデータ取得
     fecthCharacters(); // 2,3などpageの数値がわたってくる
   }, []);
-  // next押したら、fecthCharactersで次のページを呼び出す
 
+  // ページ番号(page)に基づいてキャラクター情報を非同期的に取得し、その過程でローディング状態を管理する役割
   const fecthCharacters = async (page) => {
     const apiUrl = 'https://narutodb.xyz/api/character'
     setIsLoading(true);
-    const result = await axios.get(apiUrl, { params: { page: page } }); // axios getメソッドでapiUrlにリクエストを送る
-    // 更新関数で取得したキャラクターのURLに値を更新する
-    setCharacters(result.data.characters);
+    const result = await axios.get(apiUrl, { params: { page: page, limit } }); // axios getメソッドでapiUrlにリクエストを送る
+    // console.log(result);
+    setCharacters(result.data.characters); // 更新関数で取得したキャラクターのURLに値を更新する
     setIsLoading(false);
   };
 
   return (
     <div className="container">
+      <div className="header">
+        <div className="header-content"><img src="logo.png" alt="" className='logo' /></div>
+      </div>
       { isLoading ? '読み込中...' :
         <main>
           <div className="cards-container">
@@ -64,9 +71,9 @@ function App() {
             })}
           </div>
           <div className='pager'>
-            <button className='prev'>前へ</button>
+            <button className='prev' onClick={handlePrev} disabled={page === 1}>前へ</button>
             <span className="pager-number">{page}</span>
-            <button className='next' onClick={handleNext}>次へ</button>
+            <button className='next' onClick={handleNext} disabled={limit > characters.length}>次へ</button>
           </div>
         </main>
       }
